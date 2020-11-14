ae4aa23b36e58293753b32e7e026d03ef06f9f81
alinaj2110 <anjalipraveen2110@gmail.com>
2020-11-14 16:29:17 +0530

    removing dependency on lib.v, 4 bit shifter

--- lrshift.v
+++ lrshift.v
@@ -1,3 +1,28 @@
+//basic modules
+module mux2 (input wire i0, i1, j, output wire o);
+  assign o = (j==0)?i0:i1;
+endmodule
+
+module df (input wire clk, in, output wire out);
+  reg df_out;
+  always@(posedge clk) df_out <= in;
+  assign out = df_out;
+endmodule
+
+module dfr (input wire clk, reset, in, output wire out);
+  wire reset_, df_in;
+   reset_= ~reset;
+  df_in= in & reset_;
+  df df_0 (clk, df_in, out);
+endmodule
+
+module dfrl (input wire clk, reset, load, in, output wire out);
+  wire _in;
+  mux2 mux2_0(out, in, load, _in);
+  dfr dfr_1(clk, reset, _in, out);
+endmodule
+
+//lr shifter module
 module lrshifter(input wire clk, reset, load, data, direction, output wire [3:0] out);
 
     wire [3:0]t;
