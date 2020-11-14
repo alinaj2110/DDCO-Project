5595585271196461808fbd87af1001230d2e97a3
alinaj2110 <anjalipraveen2110@gmail.com>
2020-11-14 17:21:31 +0530

    simplification/clean up of test bench

--- lrshift_tb.v
+++ lrshift_tb.v
@@ -16,21 +16,8 @@ module tb;
     end
     initial #10 load=1'b1;
     initial clk = 1'b0; always #5 clk =~ clk;
-    /*initial begin
-        test_vecs[0][1]=1'b1; test_vecs[0][0]=1'b0;
-        test_vecs[1][1]=1'b1; test_vecs[1][0]=1'b0;
-        test_vecs[2][1]=1'b1; test_vecs[2][0]=1'b0;
-        test_vecs[3][1]=1'b1; test_vecs[3][0]=1'b1;
-        test_vecs[4][1]=1'b1; test_vecs[4][0]=1'b1;
-        test_vecs[5][1]=1'b1; test_vecs[5][0]=1'b1;
-    end*/
-    initial d= 1'b0;
+    
     lrshifter lrshifter_tb(clk, reset, load, d, direction, out );
-    /*initial begin
-        #20 for(i=0;i<5;i=i+1)
-      begin #5{d,direction}=test_vecs[i]; end
-    #100 $finish;  
-    end*/
     initial begin
         #10 
         d=1'b1;
@@ -39,9 +26,6 @@ module tb;
         direction=1'b1;
         #60
         $finish;
-        //#60 d=1'b0;
-        //#40 initial {d,direction}={1,}
-
     end
     initial 
     $monitor("reset=%0b data=%b, load=%0b, direction=%0b, out=%b",reset, d, load, direction, out);
